efba3284af30f7999d359ff12235d3304c876c38
Eberhard Beilharz <eb1@sil.org>
2014-03-17 18:24:52 +0100

    [System] Add unit test for #2315
    Change-Id: I83e90faa0b7931a500566a6bf8d1bd65931ef462

--- mcs/class/System/Test/System.Configuration/ApplicationSettingsBaseTest.cs
+++ mcs/class/System/Test/System.Configuration/ApplicationSettingsBaseTest.cs
@@ -492,5 +492,37 @@ namespace MonoTests.System.Configuration {
 			Assert.AreEqual ("eclair", holder1.TestKey);
 			Assert.AreEqual ("", holder2.TestKey);
 		}
+
+		#region Bug #2315
+		class Bug2315Settings : ApplicationSettingsBase
+		{
+			public Bug2315Settings () : base ("Bug2315Settings")
+			{
+			}
+
+			[UserScopedSetting]
+			[DefaultSettingValue ("some text")]
+			public string Text {
+				get { return (string)this ["Text"]; }
+				set { this ["Text"] = value; }
+			}
+		}
+
+		[Test]
+		public void SettingSavingEventFired_Bug2315 ()
+		{
+			bool settingsSavingCalled = false;
+			var settings = new Bug2315Settings ();
+			settings.SettingsSaving += (object sender, CancelEventArgs e) => {
+				settingsSavingCalled = true;
+			};
+
+			settings.Text = DateTime.Now.ToString ();
+			settings.Save ();
+
+			Assert.IsTrue (settingsSavingCalled);
+		}
+		#endregion
+
 	}
 }
